d872c67d40509e4f83518cbfd19684e764e30bee
dlchau <dlchau@cmcglobal.vn>
2022-02-15 16:48:27 +0700

    build skeleton

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.send("hello world!");
 });
 
+router.get('/test',(req, res)=>{
+  return res.send("test")
+})
+
+router.get('*', (req, res) => {
+  return res.redirect('/')
+})
 module.exports = router;
